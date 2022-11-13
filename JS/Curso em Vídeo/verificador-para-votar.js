e1a01033c557daf9c7fa19dffa97f1afae61d521
João Vitor Victorio <103068928+JoaoVitorVictorio@users.noreply.github.com>
2022-11-13 17:01:16 -0300

    Updates

---
+++ JS/Curso em Vídeo/verificador-para-votar.js
@@ -0,0 +1,8 @@
+var idade = 65
+if (idade < 16){
+    console.log(`Com ${idade} anos você ainda não vota`)
+} else  if (idade < 18 || idade > 65){
+    console.log(`Com ${idade} anos o seu voto é opcional!`)
+} else{
+    console.log(`Com ${idade} anos o seu voto é obrigatório!`)
+    }
