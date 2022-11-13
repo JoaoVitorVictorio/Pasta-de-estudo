4e9c7939b64bbe55ba2ed084b86be901ee1eae35
João Vitor Victorio <103068928+JoaoVitorVictorio@users.noreply.github.com>
2022-11-13 15:55:10 -0300

    Updates

---
+++ JS/Curso em Vídeo/condições.js
@@ -0,0 +1,7 @@
+var vel = 45
+console.log(`O carro está a ${vel} km/h.`)
+if (vel > 50){
+    console.log(`Você ultrpassou o limite de velocidade permitida. MULTADO!`)
+} else{
+    console.log(`Digira sempre com cinto de segurança!`)
+}
