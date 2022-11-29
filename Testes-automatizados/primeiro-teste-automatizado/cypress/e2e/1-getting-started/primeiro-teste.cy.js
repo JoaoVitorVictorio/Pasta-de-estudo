cb73cdbd23633ab11475fb6865ba089d909b4b24
João Vitor Victorio <103068928+JoaoVitorVictorio@users.noreply.github.com>
2022-11-28 23:18:45 -0300

    Update primeiro-teste-automatizado

--- Testes-automatizados/primeiro-teste-automatizado/cypress/e2e/1-getting-started/primeiro-teste.cy.js
+++ Testes-automatizados/primeiro-teste-automatizado/cypress/e2e/1-getting-started/primeiro-teste.cy.js
@@ -1,13 +1,13 @@
 describe('Buscador de curso', () => {   // Descrição do teste
 
     beforeEach(() => {
-            cy.visit('https://www.youtube.com/') // O site que quero visitar no teste.
+            cy.visit('https://www.youtube.com/') //  Indicar a URL que será carregada antes de cada novo caso de teste.
         })
 
-      it('Buscar por curso ', () => {
-        cy.get('#search-form > #container').type('Curso em Video - Python'); // Escreve no campo selecionado
-        cy.get('#search-icon-legacy > yt-icon.style-scope').click(); // Clica no campo selecionado
-        cy.get('span#video-title.style-scope.ytd-playlist-renderer') // Acessa o campo selecionado
-            .should('contain', 'Curso em vídeo - Python'); // Busca pelo conteudo descrito no contain
+      it('Buscar por curso ', () => { // Criar um novo caso de teste.
+        cy.get('#search-form > #container').type('Curso em Video - Python'); // Escreve no campo selecionado.
+        cy.get('#search-icon-legacy > yt-icon.style-scope').click(); // Clica no campo selecionado.
+        cy.get('span#video-title.style-scope.ytd-playlist-renderer') //  Identificar o elemento html da página.
+            .should('contain', 'Curso em vídeo - Python'); // Busca pelo elemento identificado pelo DevTools.
       })
     })
