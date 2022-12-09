8efc8c525b0198d4bef6391955db09dcbfc04304
João Vitor Victorio <103068928+JoaoVitorVictorio@users.noreply.github.com>
2022-12-08 22:49:29 -0300

    Using new test methods

--- Testes-automatizados/Spark/cypress/e2e/test/finances.cy.js
+++ Testes-automatizados/Spark/cypress/e2e/test/finances.cy.js
@@ -28,20 +28,43 @@ describe('Dev Finances', () => {
         cy.get('#data-table tbody tr').should('have.length', 1)
     });
 
-    it('Cadastrar entrada e saída', () => {
-        //Inserindo uma entrada e uma saída
+    it('Adicionando uma entrada e uma saída', () => {
+        const entrada = 'Mesada'
+        const saida = 'Tênis'
+
+        //Inserindo uma entrada
         cy.get('#transaction .button').click()
-        cy.get('#description').type('Mesada')
+        cy.get('#description').type(entrada)
         cy.get('#amount').type(150)
         cy.get('[type=date]').type('2022-12-07')
         cy.get('button').contains('Salvar').click()
+
         //Inserindo uma saida
         cy.get('#transaction .button').click()
-        cy.get('#description').type('Tênis')
+        cy.get('#description').type(saida)
         cy.get('#amount').type(-100)
         cy.get('[type=date]').type('2022-12-08')
         cy.get('button').contains('Salvar').click()
-        //Verificando quantas tabelas tinha
-        cy.get('#data-table tbody tr').should('have.length', 2)
+
+        // Removendo uma entrada (1ª estratégia)
+        cy.get('td.description')
+        .contains(entrada)
+        .parent()
+        .find('img[onclick*=remove]')
+        .click()
+
+        // Removendo uma entrada (2ª estratégia)
+        //cy.get('[data-index="0"] > :nth-child(4) > img').click()
+
+        // Removendo uma entrada (3ª estratégia)
+        cy.get('td.description')
+        .contains(saida)
+        .siblings()
+        .children('img[onclick*=remove]')
+        .click()
+        cy.get('#data-table tbody tr').should('have.length', 0)
     });
 });
+
+//npx cypress open --config viewportWidth=411, viewportHeight=823 (Abre em diferente resuluções)
+
